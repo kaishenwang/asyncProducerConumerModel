f820a594e7a70b74db5639e3518b1b1e86bf6fef
kaishenwang <kaishenw1995@gmail.com>
2020-05-25 18:20:11 -0500

    rewrite, graceful shutdown

--- model.py
+++ model.py
@@ -1,67 +1,63 @@
 import asyncio
-import random
-import queue
 import multiprocessing
 import time
 from concurrent import futures
 
-class consumer():
-    def __init__(self, num):
+class consumer(multiprocessing.Process):
+    def __init__(self, num, queue, flag):
+        multiprocessing.Process.__init__(self)
         self.coro_num = num
+        self.q = queue
+        self.run_flag = flag
 
-    def run(self, q):
+    def run(self):
         loop = asyncio.get_event_loop()
-        loop.create_task(self.run_helper(q))
-        loop.run_forever()
-        loop.close()
+        try:
+            loop.create_task(self.run_helper())
+            loop.run_forever()
+        except KeyboardInterrupt:
+            loop.close()
 
-    async def run_helper(self, q):
-        await asyncio.gather(*(self.real_work(i, q) for i in range(self.coro_num)))
+    async def run_helper(self):
+        await asyncio.gather(*(self.real_work(i) for i in range(self.coro_num)))
 
-    async def real_work(self, id, q):
-        while True:
+    async def real_work(self, id):
+        while self.run_flag:
             try:
-                #print('try get...')
-                token = q.get_nowait()
+                token = self.q.get_nowait()
             except:
                 # Important...transfer owership to other coroutine
-                await asyncio.sleep(0.5)
+                await asyncio.sleep(0.001)
                 continue
             await asyncio.get_event_loop().run_in_executor(None, self.simulate_io_bound_work)
-            print(id, ':', token)
-
-
+            print(id, ' get ', token)
 
     def simulate_io_bound_work(self):
-        #print('reach here1')
-        time.sleep(10)
-        #print('reach here2')
+        time.sleep(5)
 
 class model():
     def __init__(self):
-        self.q = multiprocessing.Queue()
-        self.consumer_num = 20
-        self.producer = multiprocessing.Process(target=self.produce)
-        self.consumer = consumer(self.consumer_num)
-
-    def produce(self):
-        i = 0
-        while True:
-            i += 1
-            time.sleep(0.5)
-            self.q.put(i)
-
-
-    def consume(self):
-        while True:
-            if self.q.empty():
-                continue
-            token = self.q.get()
-            print(token ** 2)
+        self.q = multiprocessing.Queue(1000)
+        self.consumer_num = 10
+        self.run_flag = multiprocessing.Value('B', 1)
+        self.consumer = consumer(self.consumer_num, self.q, self.run_flag)
 
     def run(self):
-        self.producer.start()
-        self.consumer.run(self.q)
+        i = 0
+        self.consumer.start()
+        while self.run_flag:
+            try:
+                i += 1
+                time.sleep(1)
+                self.q.put(i)
+                print('Put ', i)
+            except KeyboardInterrupt:
+                print('KeyboardInterrupt caught. Exiting...')
+                quit()
+                self.consumer.join()
+        
+    def quit():
+        self.run_flag = 0
 
 
 m = model()
